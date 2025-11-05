dfa8a22a3529b0e27a9a06937b382e0a13c25a21
Emmanuel <blaemedia26@gmail.com>
2025-11-05 16:29:28 +0100

    Initial commit: added TypeScript project files

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,16 @@ interface Teacher {
   [key: string]: any; // allows additional properties
 }
 
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
 interface Director extends Teacher {
   numberOfReports: number;
 }
@@ -35,12 +45,36 @@ const director1: Director = {
 
 
 
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
+
+
+const student1 = new StudentClass("Alice", "Smith");
+console.log(student1.displayName()); 
+console.log(student1.workOnHomework()); 
 
 function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
   return `${firstName}. ${lastName}`;
 }
 
 
-console.log(printTeacher("John", "Doe")); // Output: J. Doe
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
 console.log(teacher1);
 console.log(director1);
