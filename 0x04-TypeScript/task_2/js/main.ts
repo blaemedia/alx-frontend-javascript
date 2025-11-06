5bfc07cacbcc0d0307d89dbc3c97f1d0f7158753
Emmanuel <blaemedia26@gmail.com>
2025-11-06 02:58:23 +0100

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -51,9 +51,29 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+
+
+
+// Define string literal type
+type Subjects = 'Math' | 'History';
+
+// Function to teach class
+function teachClass(todayClass: Subjects): string {
+  return `Teaching ${todayClass}`;
+}
+
+// Test cases
+console.log(teachClass('Math'));     // Teaching Math
+console.log(teachClass('History'));  // Teaching History
+
+
 // Test the function
 console.log(createEmployee(200));
 // Teacher
 console.log(createEmployee(1000));
 // Director
 console.log(createEmployee('$500'));
+
+
+
+
