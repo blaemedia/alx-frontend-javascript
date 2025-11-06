6d97e2d57fc9771b2e287c07c083d67b6b379480
Emmanuel <blaemedia26@gmail.com>
2025-11-06 06:46:05 +0100

    Ambient Namespaces fix

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,12 @@
+// Create a type RowID and set it equal to number
+type RowID =number;
+
+// Create an interface RowElement
+interface RowElement {
+  firstName:string;
+  lastName:string;
+  age?:number;
+}
+
+// Export them for use in other files
+export { RowID, RowElement };
--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -13,6 +13,15 @@ const row: RowElement = {
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
-const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+//const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
+
+
+//const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowID, updatedRow);
+
 CRUD.deleteRow(newRowID);
